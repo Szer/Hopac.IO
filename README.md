45a975f2e807acd6b43f27fb3580752b673de6ba
Ayrat Hudaygulov <ayrat.hudaygulov@arkadium.com>
2017-12-21 18:45:36 +0300

    HttpWebRequest added

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Hopac.IO
 
-Extensions for standard IO operations with Hopac Jobs.
-Without Task<T> or Async<T> overhead.
+Extensions for standard IO operations with Hopac Jobs without Task<T> or Async<T> overhead.
 
 In order to start the build process run
 
@@ -10,12 +9,14 @@ In order to start the build process run
 Paket and FAKE will do the rest. 
 Build process described in build.fsx
 
-## Whats inside
+## What's inside
     * System.IO.Stream
         * ReadToEndJob
         * ReadJob
     * System.IO.StreamReader
         * ReadToEndJob
+    * System.Net.HttpWebRequest
+        * GetResponseJob
 
 ## Requirements
 
---
+++ src/Hopac.IO/Http.fs
@@ -0,0 +1,23 @@
+namespace Hopac.IO
+
+[<AutoOpen>]
+module Net = 
+
+    open System.Net
+    open Hopac
+
+    type System.Net.HttpWebRequest with
+
+        ///**Description**
+        ///returns a response to an Internet request as a Hopac job
+        member request.GetResponseJob () =
+            let inline succeed (wr : WebResponse) : Choice<WebResponse,exn> = downcast wr |> Choice1Of2
+            let inline failure (ex : exn)         : Choice<WebResponse,exn> = Choice2Of2 ex
+            let tryEndGetResponse ar =
+                try
+                    request.EndGetResponse ar
+                    |> succeed
+                with
+                | :? WebException as wex when isNull wex.Response -> succeed wex.Response
+                | ex -> failure ex
+            Alt.fromBeginEnd request.BeginGetResponse tryEndGetResponse (fun _ -> request.Abort())
